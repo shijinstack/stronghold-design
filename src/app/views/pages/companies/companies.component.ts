dc0b81bfbba94ee333704fa63b453b90f2486f19
Shijin Gireesan <shijingireesan@Shijins-MacBook-Pro.local>
2023-04-06 00:02:55 +0530

    add company modal created

--- src/app/views/pages/companies/companies.component.ts
+++ src/app/views/pages/companies/companies.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-companies',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompaniesComponent implements OnInit {
 
-  constructor() { }
+  simpleItems: any = [];
+  simpleItems2: any = [];
+  selectedSimpleItem: any = null;
+  selectedSimpleItem2: any = null;
 
+  constructor(private modalService: NgbModal) { }
+
+  openAddCompanyModal(content: TemplateRef<any>) {
+    this.modalService.open(content, {centered: true, windowClass: 'modal_right', fullscreen: true  }).result.then((result) => {
+
+    }).catch((res) => {});
+  }
   ngOnInit(): void {
+    this.simpleItems = ['India', 'USA', 'Japan'];
+    this.simpleItems2 = ['Kerala', 'Tamilnadu', 'Karnataka'];
   }
 
 }
