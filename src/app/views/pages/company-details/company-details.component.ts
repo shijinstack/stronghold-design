7de3a23f1e3d93432009a0d70b0d1c9d56fc3312
Shijin Gireesan <shijingireesan@Shijins-MacBook-Pro.local>
2023-04-06 19:01:16 +0530

    employee details popup

--- src/app/views/pages/company-details/company-details.component.ts
+++ src/app/views/pages/company-details/company-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-company-details',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  openViewEmployeeModal(content: TemplateRef<any>) {
+    this.modalService.open(content, {centered: true, windowClass: 'modal_right full', fullscreen: true  }).result.then((result) => {
+
+    }).catch((res) => {});
   }
 
 }
